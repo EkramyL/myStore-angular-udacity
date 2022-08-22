7af1050e93f60934a248007b672b664673122fb0
Ekramy Latief <ekramyl3000@gmail.com>
2022-08-22 11:31:52 +0200

    feat: Get each product id in product detail page

--- src/app/components/product-item-detail/product-item-detail.component.ts
+++ src/app/components/product-item-detail/product-item-detail.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from 'src/app/model/product';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product-item-detail',
   templateUrl: './product-item-detail.component.html',
-  styleUrls: ['./product-item-detail.component.css']
+  styleUrls: ['./product-item-detail.component.css'],
 })
 export class ProductItemDetailComponent implements OnInit {
+  product: Product = {
+    id: 0,
+    name: '',
+    price: 0,
+    url: '',
+    description: '',
+  };
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private productService: ProductService
+  ) {}
 
   ngOnInit(): void {
+    const routeParams = this.route.snapshot.paramMap;
+    const id = Number(routeParams.get('id'));
+    this.product = this.productService.getProduct(id);
   }
-
 }
